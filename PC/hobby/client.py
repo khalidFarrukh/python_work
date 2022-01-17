7168a2467960b94c44f5a536085ad09b7ee2f592
Farrukh Khalid <khalidfarrukh951@gmail.com>
2022-01-18 02:56:09 +0500

    pc

--- PC/hobby/client.py
+++ PC/hobby/client.py
@@ -1,15 +1,13 @@
 import socket
 
-host  = "192.168.0.105"
+host = "192.168.0.105"
 port = 1025
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind(host,port)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(host, port)
 s.listen(5)
 
 while True:
-    client,caddress = s.accept()
+    client, caddress = s.accept()
     print(f"connection to {caddress} established")
     message = "Socket programming in python"
     client.send(message.encode("utf-8"))
-    
-
