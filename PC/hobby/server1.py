e958c4baa51dc0bf149dc9f54fc67296a75031bd
Farrukh Khalid <khalidfarrukh951@gmail.com>
2022-02-07 22:48:27 +0500

    pc

--- PC/hobby/server1.py
+++ PC/hobby/server1.py
@@ -18,7 +18,6 @@ import socket
 # s.close()
 
 # --------------------------------------------------------------
-# host_ip = socket.gethostbyname(socket.gethostname())
 host_ip = "192.168.0.105"
 port = 1025
 local_EP = (host_ip, port)
