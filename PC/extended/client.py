8c0ac3d5bfff6547c928b20b181e2dbc318cd3fd
Farrukh Khalid <khalidfarrukh951@gmail.com>
2022-02-21 17:55:49 +0500

    pc

---
+++ PC/extended/client.py
@@ -0,0 +1,35 @@
+import socket
+import keyboard
+import time
+import subprocess
+
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# host_name = socket.gethostname()
+# host_ip = socket.gethostbyname(host_name)
+# print(host_name, " ", host_ip)
+# port = 1025
+# s.connect((host_ip, port))
+# msg = ""
+# while True:
+#     temp = s.recv(8)
+#     if len(temp) == 0:
+#         break
+#     msg += temp.decode("utf-8")
+# print(msg)
+# s.close()
+
+# --------------------------------------------------------------
+hip = "192.168.0.105"
+port = 1025
+local_EP = (hip, port)
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+    s.connect(local_EP)
+    msg = keyboard.read_key()
+    s.send(str(msg).encode("utf-8"))
+    print(msg)
+    if msg == "esc":
+        s.close()
+        break
+    s.close()
+    time.sleep(0.1)
