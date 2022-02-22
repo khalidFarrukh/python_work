ef168dd24c19ee4171d781974eefd79ad7414a32
Farrukh Khalid <khalidfarrukh951@gmail.com>
2022-02-22 18:07:02 +0500

    pc

--- PC/extended/client.py
+++ PC/extended/client.py
@@ -1,7 +1,10 @@
 import socket
-import keyboard
+import keyboard as kb
 import time
 import subprocess
+from threading import *
+import mouse
+import pickle
 
 # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # host_name = socket.gethostname()
@@ -19,17 +22,31 @@ import subprocess
 # s.close()
 
 # --------------------------------------------------------------
+
+# hip = "192.168.0.105"
+# port = 1025
+# local_EP = (hip, port)
+# while True:
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+#     s.connect(local_EP)
+#     msg = list(mouse.get_position())  # mouse location
+#     s.send(str(msg).encode("utf-8"))
+#     print(msg)
+#     if msg == "esc":
+#         s.close()
+#         break
+#     s.close()
+#     time.sleep(0.1)
+
+# --------------------------------------------------------------
+
 hip = "192.168.0.105"
 port = 1025
 local_EP = (hip, port)
 while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     s.connect(local_EP)
-    msg = keyboard.read_key()
-    s.send(str(msg).encode("utf-8"))
-    print(msg)
-    if msg == "esc":
-        s.close()
-        break
+    msg = s.recv(1024).decode("utf-8")
+    msg = eval(msg)
+    mouse.move(msg[0], msg[1])
     s.close()
-    time.sleep(0.1)
--- PC/extended/server.py
+++ PC/extended/server.py
@@ -1,5 +1,11 @@
 from ast import Name
 import socket
+import cv2
+import numpy as np
+import pyautogui
+import keyboard as kb
+import mouse
+import time
 
 # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # host_name = socket.gethostname()
@@ -18,21 +24,55 @@ import socket
 # s.close()
 
 # --------------------------------------------------------------
+
+# hip = "192.168.0.105"
+# port = 1025
+# local_EP = (hip, port)
+# while True:
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+#     s.bind(local_EP)
+#     s.listen(10)
+#     print("waiting for a connection....")
+#     clnt, c_address = s.accept()
+#     print(f"connection to {c_address} established")
+#     print("")
+#     msg = clnt.recv(1024).decode("utf-8")
+#     print(type(msg))
+#     msg = eval(msg)
+#     print(type(msg))
+#     clnt.close()
+#     if msg == "esc":
+#         print("client closed by user")
+#     else:
+#         print(msg)
+#     s.close()
+# --------------------------------------------------------------
+
 hip = "192.168.0.105"
-port = 1025
-local_EP = (hip, port)
+port1 = 1025
+port2 = 1026
+local_EP1 = (hip, port1)
+# local_EP2 = (hip, port2)
 while True:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    s.bind(local_EP)
-    s.listen(10)
-    print("waiting for a connection....")
-    clnt, c_address = s.accept()
-    print(f"connection to {c_address} established")
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+    # s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+    s1.bind(local_EP1)
+    # s2.bind(local_EP2)
+    s1.listen(10)
+    # s2.listen(10)
+    print("waiting for a connection 1....")
+    clnt1, c_address1 = s1.accept()
+    print(f"connection to {c_address1} established")
     print("")
-    msg = clnt.recv(1024).decode("utf-8")
-    clnt.close()
-    if msg == "esc":
-        print("client closed by user")
-    else:
-        print(msg)
-    s.close()
+
+    # print("waiting for a connection 2....")
+    # clnt2, c_address2 = s2.accept()
+    # print(f"connection to {c_address2} established")
+    # print("")
+
+    msg = list(mouse.get_position())  # mouse location
+    s1.send(str(msg).encode("utf-8"))
+    s1.close()
+    time.sleep(0.1)
+
+# --------------------------------------------------------------------------
