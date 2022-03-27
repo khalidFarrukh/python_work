0a81c6b817007c3c4488439aed70de73dced7c8f
Farrukh Khalid <khalidfarrukh951@gmail.com>
2022-03-27 21:33:13 +0500

    pc

---
+++ PC/extended/try2/client.py
@@ -0,0 +1,89 @@
+import socket
+import keyboard as kb
+import time
+import subprocess
+from threading import *
+import mouse
+import pickle
+import pyautogui
+import cv2
+import numpy as np
+from PIL import ImageTk, Image
+
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# host_name = socket.gethostname()
+# host_ip = socket.gethostbyname(host_name)
+# print(host_name, " ", host_ip)
+# port = 1025
+# s.connect((host_ip, port))
+# msg = ""
+# while True:
+#     temp = s.recv(8)
+#     if len(temp) == 0:
+#         break
+#     msg += temp.decode("utf-8")
+# print(msg)
+# s.close()
+
+# --------------------------------------------------------------
+
+# hip = "192.168.0.105"
+# port = 1025
+# local_EP = (hip, port)
+# while True:
+#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+#     s.connect(local_EP)
+#     msg = list(mouse.get_position())  # mouse location
+#     s.send(str(msg).encode("utf-8"))
+#     print(msg)
+#     if msg == "esc":
+#         s.close()
+#         break
+#     s.close()
+#     time.sleep(0.1)
+
+# --------------------------------------------------------------
+
+SCREEN_SIZE = tuple(pyautogui.size())
+g_m_p_check = False
+s_s_f_check = False
+
+
+def send_screen():
+    hip = "192.168.0.107" # direct internet ip
+    #hip = "192.168.0.106" # wifi ip
+    port = 1025
+    local_s_ep = (hip, port)
+    while True:
+        local_cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+        local_cs.bind(local_s_ep)
+        local_cs.listen(10)
+        print("client to server : waiting for a connection")
+        remote_sc, r_sc_address = local_cs.accept()
+        print(f"connection to {r_sc_address} established")
+        
+        msg = "i am super man"  # mouse location 
+        remote_sc.send(pickle.dumps(msg))
+        remote_sc.close()
+        local_cs.close()
+
+
+def get_mouse_position():
+    rhip = "192.168.0.105" # direct internet ip
+    port = 1026
+    remote_s_ep = (rhip, port)
+    while True:
+        local_cc = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+        local_cc.connect(remote_s_ep)
+        temp = local_cc.recv(1024)
+        rec_msg = pickle.loads(temp)
+        print(rec_msg)
+        local_cc.close()
+
+
+t1 =Thread(target=send_screen)
+t2 =Thread(target=get_mouse_position)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
---
+++ PC/extended/try2/server.py
@@ -0,0 +1,55 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import pyautogui
+import cv2
+import pickle
+import socket
+import numpy as np
+import keyboard as kb
+import mouse
+import time
+from threading import *
+
+SCREEN_SIZE = tuple(pyautogui.size())
+def get_screen():
+    rhip = "192.168.0.107" # direct internet ip
+    #rhip = "192.168.0.106" # wifi ip
+    port = 1025
+    remote_s_ep = (rhip, port)
+    while True:
+        local_sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+        try:
+            local_sc.connect(remote_s_ep)
+            temp = local_sc.recv(1024)
+            rec_msg = pickle.loads(temp)
+            print(rec_msg)
+            local_sc.close()
+        except NameError as e:
+            print(e)
+            local_sc.close()
+            return -1
+
+def send_mouse_position():
+    hip = "192.168.0.105" # direct internet ip
+    port = 1026
+    local_s_ep = (hip, port)
+    while True:
+        local_ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+        local_ss.bind(local_s_ep)
+        local_ss.listen(10)
+        print("waiting for a connection ....")
+        remote_cs, r_cs_address = local_ss.accept()
+        print(f"connection to {r_cs_address} established")
+        print("")
+
+        msg = "i am iron man"  # mouse location
+        remote_cs.send(pickle.dumps(msg))
+        remote_cs.close()
+        local_ss.close()
+
+t1 = Thread(target=send_mouse_position)
+t2 = Thread(target=get_screen)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
