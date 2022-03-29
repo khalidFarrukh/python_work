6adb50392a48dc9acde63c8772d1c32e9705994c
Farrukh Khalid <khalidfarrukh951@gmail.com>
2022-03-29 17:40:53 +0500

    pc

--- PC/extended/try4/server.py
+++ PC/extended/try4/server.py
@@ -10,17 +10,22 @@ import mouse
 import time
 from threading import*
 import multiprocessing
-from pynput.mouse import Listener,mouse,Controller
+from pynput.mouse import Listener,Button,Controller
 import os
 os.system("cls")
 
 mouse_hip = "192.168.0.105" # direct internet ip
-mouse_port = 1026
-mouse_local_s_ep = (mouse_hip, mouse_port)
+when_mouse_move_port = 1026
+when_mouse_move_local_s_ep = (mouse_hip, when_mouse_move_port)
+when_mouse_click_port = 1027
+when_mouse_click_local_s_ep = (mouse_hip, when_mouse_click_port)
+when_mouse_scroll_port = 1028
+when_mouse_scroll_local_s_ep = (mouse_hip, when_mouse_scroll_port)
 
-def on_move(x, y):
+
+def when_mouse_move(x, y):
     mouse_local_ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    mouse_local_ss.bind(mouse_local_s_ep)
+    mouse_local_ss.bind(when_mouse_move_local_s_ep)
     mouse_local_ss.listen(10)
     print("\033[1;37;40m")
     print("waiting for a connection ....")
@@ -34,9 +39,9 @@ def on_move(x, y):
     mouse_local_ss.close()
     
 
-def on_click(x, y, button, pressed):
+def when_mouse_click(x, y, button, pressed):
     mouse_local_ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    mouse_local_ss.bind(mouse_local_s_ep)
+    mouse_local_ss.bind(when_mouse_click_local_s_ep)
     mouse_local_ss.listen(10)
     print("\033[1;37;40m")
     print("waiting for a connection ....")
@@ -57,9 +62,9 @@ def on_click(x, y, button, pressed):
     mouse_remote_cs.close()
     mouse_local_ss.close()
 
-def on_scroll(x, y, dx, dy):
+def when_mouse_scroll(x, y, dx, dy):
     mouse_local_ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    mouse_local_ss.bind(mouse_local_s_ep)
+    mouse_local_ss.bind(when_mouse_scroll_local_s_ep)
     mouse_local_ss.listen(10)
     print("\033[1;37;40m")
     print("waiting for a connection ....")
@@ -67,16 +72,15 @@ def on_scroll(x, y, dx, dy):
     print(f"connection to {m_r_cs_address} established")
     print("")
     print("\033[1;37;40m")
-    print(x, y, dx, dy)
-    m_pos_scroll = list(x,y,dx,dy)  # mouse location
-    mouse_remote_cs.send(pickle.dumps(m_pos_scroll))
+    # print(x, y, dx, dy)
+    m_scroll = list(dx,dy)  # mouse location
+    mouse_remote_cs.send(pickle.dumps(m_scroll))
     mouse_remote_cs.close()
     mouse_local_ss.close()
 
 def send_mouse_event():
-    with Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
+    with Listener(on_move=when_mouse_move, on_click=when_mouse_click, on_scroll=when_mouse_scroll) as listener:
         listener.join()
-
 if __name__ == "__main__":
     multiprocessing.freeze_support()
     p1 = multiprocessing.Process(target=send_mouse_event,args=())
