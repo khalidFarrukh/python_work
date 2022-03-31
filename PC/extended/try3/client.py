634f146a7373a3577210c1734fde8caeda78eb49
Farrukh Khalid <khalidfarrukh951@gmail.com>
2022-04-01 02:40:24 +0500

    pc

--- PC/extended/try3/client.py
+++ PC/extended/try3/client.py
@@ -1,4 +1,6 @@
 import socket
+from tokenize import Name
+from warnings import catch_warnings
 import click
 import keyboard as kb
 import time
@@ -14,36 +16,36 @@ import multiprocessing
 
 SCREEN_SIZE = tuple(pyautogui.size())
 
-# def send_screen():
-#     hip = "192.168.0.107" # direct internet ip
-#     #hip = "192.168.0.106" # wifi ip
-#     port = 1025
-#     local_s_ep = (hip, port)
-#     while True:
-#         local_cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-#         local_cs.bind(local_s_ep)
-#         local_cs.listen(10)
-#         print("client to server : waiting for a connection")
-#         remote_sc, r_sc_address = local_cs.accept()
-#         print(f"connection to {r_sc_address} established")
-#         print("")
-#         frame = Image.Image.convert(pyautogui.screenshot(),'L')
-#         remote_sc.send(pickle.dumps(frame))
-#         remote_sc.close()
-#         local_cs.close()
+def send_screen():
+    hip = "192.168.0.107" # direct internet ip
+    #hip = "192.168.0.106" # wifi ip
+    port = 1025
+    local_s_ep = (hip, port)
+    while True:
+        local_cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+        local_cs.bind(local_s_ep)
+        local_cs.listen(10)
+        print("client to server : waiting for a connection")
+        remote_sc, r_sc_address = local_cs.accept()
+        print(f"connection to {r_sc_address} established")
+        print("")
+        frame = Image.Image.convert(pyautogui.screenshot(),'L')
+        remote_sc.send(pickle.dumps(frame))
+        remote_sc.close()
+        local_cs.close()
 
 
-# def get_mouse_position():
-#     rhip = "192.168.0.105" # direct internet ip
-#     port = 1026
-#     remote_s_ep = (rhip, port)
-#     while True:
-#         local_cc = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-#         local_cc.connect(remote_s_ep)
-#         temp = local_cc.recv(1024)
-#         m_pos = pickle.loads(temp)
-#         mouse.move(m_pos[0], m_pos[1])
-#         local_cc.close()
+def get_mouse_position():
+    rhip = "192.168.0.105" # direct internet ip
+    port = 1026
+    remote_s_ep = (rhip, port)
+    while True:
+        local_cc = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+        local_cc.connect(remote_s_ep)
+        temp = local_cc.recv(1024)
+        m_pos = pickle.loads(temp)
+        mouse.move(m_pos[0], m_pos[1])
+        local_cc.close()
 
 def get_mouse_event():
     rhip = "192.168.0.105" # direct internet ip
@@ -52,35 +54,31 @@ def get_mouse_event():
     while True:
         local_cc = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
         local_cc.connect(remote_s_ep)
-        try:
-            temp = local_cc.recv(1024)
-            if len(temp)>0:
-                event = pickle.loads(temp)
-                if event == "r":
-                    # mouse.click('right')
-                    print("                                  right")
-                if event=="l":
-                    # mouse.click('left')
-                    print("left")
-                if event == "m":
-                    # mouse.click('middle')
-                    print("             middle")
-            time.sleep(0.1)
-            local_cc.close()
-        except NameError as e:
-            print("error : ",e)
-            local_cc.close()
+        temp = local_cc.recv(1024)
+        if len(temp)>0:
+            event = pickle.loads(temp)
+            if event == "r":
+                mouse.click('right')
+                print("                                  right")
+            if event=="l":
+                mouse.click('left')
+                print("left")
+            if event == "m":
+                mouse.click('middle')
+                print("             middle")
+        local_cc.close()
+        # time.sleep(0.1)
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()
-    # p1 = multiprocessing.Process(target=send_screen,args=())
-    # p2 = multiprocessing.Process(target=get_mouse_position,args=())
+    p1 = multiprocessing.Process(target=send_screen,args=())
+    p2 = multiprocessing.Process(target=get_mouse_position,args=())
     p3 = multiprocessing.Process(target=get_mouse_event,args=())
-    # p1.start()
-    # p2.start()
+    p1.start()
+    p2.start()
     p3.start()
-    # p1.join()
-    # p2.join()
+    p1.join()
+    p2.join()
     p3.join()
     while True:
         continue
--- PC/extended/try3/server.py
+++ PC/extended/try3/server.py
@@ -16,57 +16,57 @@ import win32api
 
 SCREEN_SIZE = tuple(pyautogui.size())
 
-# def get_screen():
-#     root = Tk()
-#     frame = pyautogui.screenshot()
-#     image2 = ImageTk.PhotoImage(frame)
-#     panel = Label(root,image=image2)
-#     panel.pack(side="bottom", fill="both", expand="yes")
-#     root.attributes('-fullscreen', True)
-#     rhip = "192.168.0.107" # direct internet ip
-#     #rhip = "192.168.0.106" # wifi ip
-#     port = 1025
-#     remote_s_ep = (rhip, port)
-#     while True:
-#         local_sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-#         try:
-#             local_sc.connect(remote_s_ep)
-#             temp = b""
-#             while True:
-#                 packet = local_sc.recv(1048576)
-#                 if not packet:
-#                     break
-#                 temp += packet
-#             frame = pickle.loads(temp)
-#             image2 = ImageTk.PhotoImage(frame)
-#             panel.configure(image=image2)
-#             panel.image = image2
-#             root.attributes('-fullscreen', True)
-#             root.update()
-#             if kb.is_pressed("esc"):
-#                 break
-#             local_sc.close()
-#         except NameError as e:
-#             print(e)
-#             local_sc.close()
-#             return -1
+def get_screen():
+    root = Tk()
+    frame = pyautogui.screenshot()
+    image2 = ImageTk.PhotoImage(frame)
+    panel = Label(root,image=image2)
+    panel.pack(side="bottom", fill="both", expand="yes")
+    root.attributes('-fullscreen', True)
+    rhip = "192.168.0.107" # direct internet ip
+    #rhip = "192.168.0.106" # wifi ip
+    port = 1025
+    remote_s_ep = (rhip, port)
+    while True:
+        local_sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+        try:
+            local_sc.connect(remote_s_ep)
+            temp = b""
+            while True:
+                packet = local_sc.recv(1048576)
+                if not packet:
+                    break
+                temp += packet
+            frame = pickle.loads(temp)
+            image2 = ImageTk.PhotoImage(frame)
+            panel.configure(image=image2)
+            panel.image = image2
+            root.attributes('-fullscreen', True)
+            root.update()
+            if kb.is_pressed("esc"):
+                break
+            local_sc.close()
+        except NameError as e:
+            print(e)
+            local_sc.close()
+            return -1
 
-# def send_mouse_position():
-#     hip = "192.168.0.105" # direct internet ip
-#     port = 1026
-#     local_s_ep = (hip, port)
-#     while True:
-#         local_ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-#         local_ss.bind(local_s_ep)
-#         local_ss.listen(10)
-#         print("waiting for a connection ....")
-#         remote_cs, r_cs_address = local_ss.accept()
-#         print(f"connection to {r_cs_address} established")
-#         print("")
-#         m_pos = list(mouse.get_position())  # mouse location
-#         remote_cs.send(pickle.dumps(m_pos))
-#         remote_cs.close()
-#         local_ss.close()
+def send_mouse_position():
+    hip = "192.168.0.105" # direct internet ip
+    port = 1026
+    local_s_ep = (hip, port)
+    while True:
+        local_ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+        local_ss.bind(local_s_ep)
+        local_ss.listen(10)
+        print("waiting for a connection ....")
+        remote_cs, r_cs_address = local_ss.accept()
+        print(f"connection to {r_cs_address} established")
+        print("")
+        m_pos = list(mouse.get_position())  # mouse location
+        remote_cs.send(pickle.dumps(m_pos))
+        remote_cs.close()
+        local_ss.close()
 
 # def send_mouse_event():
 #     hip = "192.168.0.105" # direct internet ip
@@ -94,38 +94,55 @@ def send_mouse_event():
     port = 1027
     local_s_ep = (hip, port)
     while True:
-        local_ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-        local_ss.bind(local_s_ep)
-        local_ss.listen(10)
-        print("waiting for a connection ....")
-        remote_cs, r_cs_address = local_ss.accept()
-        print(f"connection to {r_cs_address} established")
-        print("")
         left_click_event = win32api.GetKeyState(0x01)
         right_click_event = win32api.GetKeyState(0x02)
         middle_click_event = win32api.GetKeyState(0x04)
         if left_click_event<0:
             data = "l"
+            local_ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+            local_ss.bind(local_s_ep)
+            local_ss.listen(10)
+            print("waiting for a connection ....")
+            remote_cs, r_cs_address = local_ss.accept()
+            print(f"connection to {r_cs_address} established")
+            print("")
             remote_cs.send(pickle.dumps(data))
+            remote_cs.close()
+            local_ss.close()
         if right_click_event<0:
             data = "r"
+            local_ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+            local_ss.bind(local_s_ep)
+            local_ss.listen(10)
+            print("waiting for a connection ....")
+            remote_cs, r_cs_address = local_ss.accept()
+            print(f"connection to {r_cs_address} established")
+            print("")
             remote_cs.send(pickle.dumps(data))
+            remote_cs.close()
+            local_ss.close()
         if middle_click_event<0:
             data = "m"
+            local_ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+            local_ss.bind(local_s_ep)
+            local_ss.listen(10)
+            print("waiting for a connection ....")
+            remote_cs, r_cs_address = local_ss.accept()
+            print(f"connection to {r_cs_address} established")
+            print("")
             remote_cs.send(pickle.dumps(data))
-        time.sleep(0.1)
-        remote_cs.close()
-        local_ss.close()
+            remote_cs.close()
+            local_ss.close()
 
 if __name__ == "__main__":
     os.system("cls")
     multiprocessing.freeze_support()
-    # p1 = multiprocessing.Process(target=send_mouse_position,args=())
-    # p2 = multiprocessing.Process(target=get_screen,args=())
+    p1 = multiprocessing.Process(target=send_mouse_position,args=())
+    p2 = multiprocessing.Process(target=get_screen,args=())
     p3 = multiprocessing.Process(target=send_mouse_event,args=())
-    # p1.start()
-    # p2.start()
+    p1.start()
+    p2.start()
     p3.start()
-    # p1.join()
-    # p2.join()
+    p1.join()
+    p2.join()
     p3.join()
